f952af2736b68584ad60fc5c98f8cbe4d0f95c61
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-17 16:31:06 +0300

    finish extra exercise

--- extra/main.js
+++ extra/main.js
@@ -4,7 +4,6 @@ const allUsers = [
   ];
   
   const createUser = function (username, password) {
-    //   return "hi"
     for(let i=0;i<allUsers.length;i++)
     {
         if(allUsers[i].username===username)
@@ -19,3 +18,20 @@ const allUsers = [
     
   };
 
+
+
+  const sliceArray = function (array, control) {
+    let newArray=[];
+    let ch = Math.floor(array.length/control);
+    
+    for(let i=0;i<ch;i++)
+    {
+     let items = array.splice(0,control)
+     newArray.push(items) 
+    }
+    if(array.length){
+      newArray.push(array)
+    }
+    
+    return newArray;
+  };
